262366c5eb0832ce7b3af07705fc9429c662027b
Imeime19 <uyoimeime@gmail.com>
2021-01-15 17:49:00 +0100

    Added the marker component to the data in the scatter plot

--- basicplotly.py
+++ basicplotly.py
@@ -6,7 +6,13 @@ np.random.seed(42)
 random_x = np.random.randint(1,101,100)
 random_y = np.random.randint(1,101,100)
 
-data = [go.Scatter(x=random_x, y=random_y,mode='markers')]
+data = [go.Scatter(x=random_x, y=random_y,mode='markers',
+marker=dict(
+    size=12,
+    color='rgb(51,204,153)',
+    symbol='pentagon',
+    line={'width':2}
+))]
 
 
 layout = go.Layout(title='Hello First plot',
